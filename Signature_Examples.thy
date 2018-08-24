bc03e669ec904528510110e357c3b02f37fbb5b9
Alexander Maletzky <amaletzk@risc.uni-linz.ac.at>
2018-08-24 11:03:15 +0200

    Improved setup of term orders in computations with signature-based algorithms

--- Signature_Examples.thy
+++ Signature_Examples.thy
@@ -6,7 +6,7 @@ theory Signature_Examples
   imports Signature_Based Polynomials.MPoly_Type_Class_OAlist Groebner_Bases.Code_Target_Rat Print
 begin
 
-subsection \<open>Benchmarks\<close>
+subsection \<open>Benchmark Problems\<close>
 
 (* TODO: Move to separate "Benchmarks" theory. *)
 
@@ -62,106 +62,158 @@ qed
 instance pp :: (countable, add_wellorder) quasi_pm_powerprod
   by (standard, intro exI conjI, fact dickson_grading_varnum_pp, fact hom_grading_varnum_pp)
 
-(* TODO: Replace class "nat_pp_term" by this. *)
-class nat_pp_term2 = nat_pp_term +
-  assumes snd_rep_nat_term: "snd (rep_nat_term x) = 0"
+subsubsection \<open>Projections of Term Orders to Orders on Power-Products\<close>
 
-instance pp :: (nat, nat) nat_pp_term2
-  by (intro_classes, simp add: rep_nat_term_pp_def)
+definition proj_comp :: "(('a::nat, 'b::nat) pp \<times> nat) nat_term_order \<Rightarrow> ('a, 'b) pp \<Rightarrow> ('a, 'b) pp \<Rightarrow> order"
+  where "proj_comp cmp = (\<lambda>x y. nat_term_compare cmp (x, 0) (y, 0))"
 
-definition pot_comp' :: "'a::nat_pp_term2 nat_term_order \<Rightarrow> ('a \<times> nat) \<Rightarrow> ('a \<times> nat) \<Rightarrow> order"
-  where "pot_comp' cmp = (\<lambda>x y. case comparator_of (snd x) (snd y) of Eq \<Rightarrow> nat_term_compare cmp (fst x) (fst y) | val \<Rightarrow> val)"
+definition proj_ord :: "(('a::nat, 'b::nat) pp \<times> nat) nat_term_order \<Rightarrow> ('a, 'b) pp nat_term_order"
+  where "proj_ord cmp = Abs_nat_term_order (proj_comp cmp)"
 
-definition pot :: "'a::{comm_powerprod,nat_pp_term2,nat_pp_compare} nat_term_order \<Rightarrow> ('a \<times> nat) nat_term_order"
-  where "pot cmp = Abs_nat_term_order (pot_comp' cmp)"
+text \<open>In principle, @{const proj_comp} and @{const proj_ord} could be defined more generally on type
+  @{typ "'a \<times> nat"}, but then @{typ 'a} would have to belong to some new type-class which is the
+  intersection of @{class nat_pp_term} and @{class nat_pp_compare} and additionally requires
+  @{prop "rep_nat_term x = (rep_nat_pp x, 0)"}.\<close>
 
-lemma nat_term_compare_pot: "nat_term_compare (pot cmp) = pot_comp' cmp"
-  unfolding pot_def
-proof (rule nat_term_compare_Abs_nat_term_order_id)
-  show "comparator (pot_comp' cmp)" unfolding pot_comp'_def using comparator_of comparator_nat_term_compare
-    by (rule comparator_lexicographic) (simp add: prod_eqI)
-next
-  have 1: "fst u = fst v" if "fst (rep_nat_term u) = fst (rep_nat_term v)" for u v :: "'a \<times> nat"
-  proof (rule rep_nat_pp_inj)
-    from that show "rep_nat_pp (fst u) = rep_nat_pp (fst v)" by (simp add: rep_nat_term_prod_def)
-  qed
-  have 2: "snd u = snd v" if "snd (rep_nat_term u) = snd (rep_nat_term v)" for u v :: "'a \<times> nat"
-  proof (rule rep_inj)
-    from that show "rep_nat (snd u) = rep_nat (snd v)" by (simp add: rep_nat_term_prod_def)
-  qed
-  show "nat_term_comp (pot_comp' cmp)"
-  proof (rule nat_term_compI)
-    fix u v :: "'a \<times> nat"
-    assume "snd (rep_nat_term u) = snd (rep_nat_term v)"
-    hence a: "snd u = snd v" by (rule 2)
-    assume "fst (rep_nat_term u) = 0"
-    hence "rep_nat_pp (fst u) = rep_nat_pp 0" by (simp add: rep_nat_term_prod_def zero_pp)
-    hence "fst u = 0" by (rule rep_nat_pp_inj)
-    note nat_term_comp_nat_term_compare
-    moreover have "snd (rep_nat_term (fst u)) = snd (rep_nat_term (fst v))" by (simp only: snd_rep_nat_term)
-    moreover have "fst (rep_nat_term (fst u)) = 0" by (simp add: \<open>fst u = 0\<close> rep_nat_term_zero)
-    ultimately have "nat_term_compare cmp (fst u) (fst v) \<noteq> Gt" by (rule nat_term_compD1)
-    thus "pot_comp' cmp u v \<noteq> Gt" using a by (simp add: pot_comp'_def)
+lemma comparator_proj_comp: "comparator (proj_comp cmp)"
+proof -
+  interpret cmp: comparator "nat_term_compare cmp" by (rule comparator_nat_term_compare)
+  show ?thesis unfolding proj_comp_def
+  proof
+    fix x y :: "('a, 'b) pp"
+    show "invert_order (nat_term_compare cmp (x, 0) (y, 0)) = nat_term_compare cmp (y, 0) (x, 0)"
+      by (simp only: cmp.sym)
   next
-    fix u v :: "'a \<times> nat"
-    assume "snd (rep_nat_term u) < snd (rep_nat_term v)"
-    hence "rep_nat (snd u) < rep_nat (snd v)" by (simp add: rep_nat_term_prod_def)
-    hence "snd u < snd v" by (simp only: ord_iff[symmetric])
-    thus "pot_comp' cmp u v = Lt" by (simp add: pot_comp'_def comparator_of_def)
+    fix x y :: "('a, 'b) pp"
+    assume "nat_term_compare cmp (x, 0) (y, 0) = Eq"
+    hence "(x, 0) = (y, 0::nat)" by (rule cmp.weak_eq)
+    thus "x = y" by simp
   next
-    fix t u v :: "'a \<times> nat"
-    have "nat_term_compare cmp (fst u) (fst v) = Lt \<Longrightarrow>
-            nat_term_compare cmp (splus (fst t) (fst u)) (splus (fst t) (fst v)) = Lt"
-      using nat_term_comp_nat_term_compare by (rule nat_term_compD3)
-    moreover assume "pot_comp' cmp u v = Lt"
-    ultimately show "pot_comp' cmp (splus t u) (splus t v) = Lt"
-      by (simp add: pot_comp'_def splus_prod_def pprod.splus_def splus_pp_term split: order.splits)
-  next
-    fix u v a b :: "'a \<times> nat"
-    assume "fst (rep_nat_term u) = fst (rep_nat_term a)" and "fst (rep_nat_term v) = fst (rep_nat_term b)"
-      and "snd (rep_nat_term u) = snd (rep_nat_term v)" and "snd (rep_nat_term a) = snd (rep_nat_term b)"
-    hence 3: "fst u = fst a" and 4: "fst v = fst b" and 5: "snd u = snd v" and 6: "snd a = snd b"
-      by (auto dest!: 1 2)
-    note comparator_nat_term_compare nat_term_comp_nat_term_compare
-    moreover have "fst (rep_nat_term (fst u)) = fst (rep_nat_term (fst a))" by (simp only: 3)
-    moreover have "fst (rep_nat_term (fst v)) = fst (rep_nat_term (fst b))" by (simp only: 4)
-    moreover have "snd (rep_nat_term (fst u)) = snd (rep_nat_term (fst v))" by (simp only: snd_rep_nat_term)
-    moreover have "snd (rep_nat_term (fst a)) = snd (rep_nat_term (fst b))" by (simp only: snd_rep_nat_term)
-    ultimately have "nat_term_compare cmp (fst u) (fst v) = nat_term_compare cmp (fst a) (fst b)"
-      by (rule nat_term_compD4')
-    moreover assume "pot_comp' cmp a b = Lt"
-    ultimately show "pot_comp' cmp u v = Lt"
-      by (simp add: pot_comp'_def 5 6 split: order.splits)
+    fix x y z :: "('a, 'b) pp"
+    assume "nat_term_compare cmp (x, 0) (y, 0) = Lt" and "nat_term_compare cmp (y, 0) (z, 0) = Lt"
+    thus "nat_term_compare cmp (x, 0) (z, 0) = Lt" by (rule cmp.trans)
   qed
 qed
 
-lemma le_of_nat_term_order_pot [code_unfold]:
-  "le_of_nat_term_order (pot cmp_pp) = le_of_comp (pot_comp' cmp_pp)"
-  by (intro ext, simp add: le_of_nat_term_order nat_term_compare_pot le_of_comp_def split: order.split)
+lemma nat_term_comp_proj_comp: "nat_term_comp (proj_comp cmp)"
+proof -
+  have 1: "fst (rep_nat_term (u, i)) = rep_nat_pp u" for u::"('a, 'b) pp" and i::nat
+    by (simp add: rep_nat_term_prod_def)
+  have 2: "snd (rep_nat_term (u, i)) = i" for u::"('a, 'b) pp" and i::nat
+    by (simp add: rep_nat_term_prod_def rep_nat_nat_def)
+  show ?thesis
+  proof (rule nat_term_compI)
+    fix u v :: "('a, 'b) pp"
+    assume a: "fst (rep_nat_term u) = 0"
+    note nat_term_comp_nat_term_compare
+    moreover have "snd (rep_nat_term (u, 0::nat)) = snd (rep_nat_term (v, 0::nat))" by (simp only: 2)
+    moreover from a have "fst (rep_nat_term (u, 0::nat)) = 0" by (simp add: 1 rep_nat_term_pp_def)
+    ultimately have "nat_term_compare cmp (u, 0) (v, 0) \<noteq> Gt" by (rule nat_term_compD1)
+    thus "proj_comp cmp u v \<noteq> Gt" by (simp add: proj_comp_def)
+  next
+    fix u v :: "('a, 'b) pp"
+    assume "snd (rep_nat_term u) < snd (rep_nat_term v)"
+    thus "proj_comp cmp u v = Lt" by (simp add: rep_nat_term_pp_def)
+  next
+    fix t u v :: "('a, 'b) pp"
+    assume "proj_comp cmp u v = Lt"
+    hence "nat_term_compare cmp (u, 0) (v, 0) = Lt" by (simp add: proj_comp_def)
+    with nat_term_comp_nat_term_compare have "nat_term_compare cmp (splus (t, 0) (u, 0)) (splus (t, 0) (v, 0)) = Lt"
+      by (rule nat_term_compD3)
+    thus "proj_comp cmp (splus t u) (splus t v) = Lt"
+      by (simp add: proj_comp_def splus_prod_def pprod.splus_def splus_pp_term)
+  next
+    fix u v a b :: "('a, 'b) pp"
+    assume u: "fst (rep_nat_term u) = fst (rep_nat_term a)" and v: "fst (rep_nat_term v) = fst (rep_nat_term b)"
+      and a: "proj_comp cmp a b = Lt"
+    note nat_term_comp_nat_term_compare
+    moreover from u have "fst (rep_nat_term (u, 0::nat)) = fst (rep_nat_term (a, 0::nat))"
+      by (simp add: 1 rep_nat_term_pp_def)
+    moreover from v have "fst (rep_nat_term (v, 0::nat)) = fst (rep_nat_term (b, 0::nat))"
+      by (simp add: 1 rep_nat_term_pp_def)
+    moreover have "snd (rep_nat_term (u, 0::nat)) = snd (rep_nat_term (v, 0::nat))"
+      and "snd (rep_nat_term (a, 0::nat)) = snd (rep_nat_term (b, 0::nat))" by (simp_all only: 2)
+    moreover from a have "nat_term_compare cmp (a, 0) (b, 0) = Lt" by (simp add: proj_comp_def)
+    ultimately have "nat_term_compare cmp (u, 0) (v, 0) = Lt" by (rule nat_term_compD4)
+    thus "proj_comp cmp u v = Lt" by (simp add: proj_comp_def)
+  qed
+qed
 
-lemma lt_of_nat_term_order_pot [code_unfold]:
-  "lt_of_nat_term_order (pot cmp_pp) = lt_of_comp (pot_comp' cmp_pp)"
-  by (intro ext, simp add: lt_of_nat_term_order nat_term_compare_pot lt_of_comp_def split: order.split)
+corollary nat_term_compare_proj_ord: "nat_term_compare (proj_ord cmp) = proj_comp cmp"
+  unfolding proj_ord_def using comparator_proj_comp nat_term_comp_proj_comp
+  by (rule nat_term_compare_Abs_nat_term_order_id)
 
-locale qpm_nat_inf_term = gd_nat_term "\<lambda>x. x" "\<lambda>x. x" "pot cmp_pp"
-  for cmp_pp::"'a::{nat_pp_term2,quasi_pm_powerprod,nat_pp_compare} nat_term_order"
+lemma proj_ord_LEX [code]: "proj_ord LEX = LEX"
+proof -
+  have "nat_term_compare (proj_ord LEX) = nat_term_compare LEX"
+    by (auto simp: nat_term_compare_proj_ord nat_term_compare_LEX proj_comp_def lex_comp
+        lex_comp_aux_def rep_nat_term_prod_def rep_nat_term_pp_def intro!: ext split: order.split)
+  thus ?thesis by (simp only: nat_term_compare_inject)
+qed
+
+lemma proj_ord_DRLEX [code]: "proj_ord DRLEX = DRLEX"
+proof -
+  have "nat_term_compare (proj_ord DRLEX) = nat_term_compare DRLEX"
+    by (auto simp: nat_term_compare_proj_ord nat_term_compare_DRLEX proj_comp_def deg_comp pot_comp
+        lex_comp lex_comp_aux_def rep_nat_term_prod_def rep_nat_term_pp_def intro!: ext split: order.split)
+  thus ?thesis by (simp only: nat_term_compare_inject)
+qed
+
+lemma proj_ord_DEG [code]: "proj_ord (DEG to) = DEG (proj_ord to)"
+proof -
+  have "nat_term_compare (proj_ord (DEG to)) = nat_term_compare (DEG (proj_ord to))"
+    by (simp add: nat_term_compare_proj_ord nat_term_compare_DEG proj_comp_def deg_comp
+        rep_nat_term_prod_def rep_nat_term_pp_def)
+  thus ?thesis by (simp only: nat_term_compare_inject)
+qed
+
+lemma proj_ord_POT [code]: "proj_ord (POT to) = proj_ord to"
+proof -
+  have "nat_term_compare (proj_ord (POT to)) = nat_term_compare (proj_ord to)"
+    by (simp add: nat_term_compare_proj_ord nat_term_compare_POT proj_comp_def pot_comp
+        rep_nat_term_prod_def rep_nat_term_pp_def)
+  thus ?thesis by (simp only: nat_term_compare_inject)
+qed
+
+subsubsection \<open>Locale Interpretation\<close>
+
+locale qpm_nat_inf_term = gd_nat_term "\<lambda>x. x" "\<lambda>x. x" to
+  for to::"(('a::nat, 'b::nat) pp \<times> nat) nat_term_order"
 begin
 
 sublocale aux: qpm_inf_term "\<lambda>x. x" "\<lambda>x. x"
-        "le_of_nat_term_order cmp_pp"
-        "lt_of_nat_term_order cmp_pp"
-        "le_of_nat_term_order (pot cmp_pp)"
-        "lt_of_nat_term_order (pot cmp_pp)"
-  apply intro_locales
-  subgoal using linorder_le_of_nat_term_order[of cmp_pp] by (simp add: class.linorder_def class.order_def)
-  subgoal using linorder_le_of_nat_term_order[of cmp_pp] by (simp add: class.linorder_def class.order_def)
-  subgoal using linorder_le_of_nat_term_order[of cmp_pp] by (simp add: class.linorder_def class.order_def)
-  subgoal by (unfold_locales, fact le_of_nat_term_order_zero_min, auto dest: le_of_nat_term_order_plus_monotone simp: ac_simps)
-  subgoal apply unfold_locales
-    subgoal by (simp add: le_of_nat_term_order nat_term_compare_splus splus_eq_splus)
-    subgoal by (simp add: le_of_nat_term_order nat_term_compare_pot pot_comp'_def comparator_of_def split: order.split)
-    done
-  done
+        "le_of_nat_term_order (proj_ord to)"
+        "lt_of_nat_term_order (proj_ord to)"
+        "le_of_nat_term_order to"
+        "lt_of_nat_term_order to"
+proof intro_locales
+(*
+  show "class.preorder (le_of_nat_term_order (proj_ord to)) (lt_of_nat_term_order (proj_ord to))"
+    and "class.order_axioms (le_of_nat_term_order (proj_ord to))"
+    and "class.linorder_axioms (le_of_nat_term_order (proj_ord to))"
+    using linorder_le_of_nat_term_order[of "proj_ord to"] by (simp_all add: class.linorder_def class.order_def)
+next*)
+  show "ordered_powerprod_axioms (le_of_nat_term_order (proj_ord to))"
+    by (unfold_locales, fact le_of_nat_term_order_zero_min, auto dest: le_of_nat_term_order_plus_monotone simp: ac_simps)
+next
+  show "ordered_term_axioms (\<lambda>x. x) (\<lambda>x. x) (le_of_nat_term_order (proj_ord to)) (le_of_nat_term_order to)"
+  proof
+    fix v w t
+    assume "le_of_nat_term_order to v w"
+    thus "le_of_nat_term_order to (local.splus t v) (local.splus t w)"
+      by (simp add: le_of_nat_term_order nat_term_compare_splus splus_eq_splus)
+  next
+    fix v w
+    assume "le_of_nat_term_order (proj_ord to) (pp_of_term v) (pp_of_term w)"
+      and "component_of_term v \<le> component_of_term w"
+    hence "nat_term_compare to (fst v, 0) (fst w, 0) \<noteq> Gt" and "snd v \<le> snd w"
+      by (simp_all add: le_of_nat_term_order nat_term_compare_proj_ord proj_comp_def)
+    from comparator_nat_term_compare nat_term_comp_nat_term_compare _ _ _ _ this(1)
+    have "nat_term_compare to v w \<noteq> Gt"
+      by (rule nat_term_compD4'') (simp_all add: rep_nat_term_prod_def ord_iff[symmetric] \<open>snd v \<le> snd w\<close>)
+    thus "le_of_nat_term_order to v w" by (simp add: le_of_nat_term_order)
+  qed
+qed
 
 end
 
@@ -174,16 +226,16 @@ definition splus_pprod :: "('a::nat, 'b::nat) pp \<Rightarrow> _"
 definition adds_term_pprod :: "(('a::nat, 'b::nat) pp \<times> _) \<Rightarrow> _"
   where "adds_term_pprod = pprod.adds_term"
 
-global_interpretation pprod': qpm_nat_inf_term cmp_pp
+global_interpretation pprod': qpm_nat_inf_term to
   rewrites "pprod.pp_of_term = fst"
   and "pprod.component_of_term = snd"
   and "pprod.splus = splus_pprod"
   and "pprod.adds_term = adds_term_pprod"
   and "punit.monom_mult = monom_mult_punit"
-  and "pprod'.aux.punit.lt = lt_punit cmp_pp"
-  and "pprod'.aux.punit.lc = lc_punit cmp_pp"
-  and "pprod'.aux.punit.tail = tail_punit cmp_pp"
-  for cmp_pp :: "('a::nat, 'b::nat) pp nat_term_order"
+  and "pprod'.aux.punit.lt = lt_punit (proj_ord to)"
+  and "pprod'.aux.punit.lc = lc_punit (proj_ord to)"
+  and "pprod'.aux.punit.tail = tail_punit (proj_ord to)"
+  for to :: "(('a::nat, 'b::nat) pp \<times> nat) nat_term_order"
   defines max_pprod = pprod'.ord_term_lin.max
   and Koszul_syz_sigs_aux_pprod = pprod'.aux.Koszul_syz_sigs_aux
   and Koszul_syz_sigs_pprod = pprod'.aux.Koszul_syz_sigs
@@ -226,35 +278,39 @@ lemma compute_splus_pprod [code]: "splus_pprod t (s, i) = (t + s, i)"
   by (simp add: splus_pprod_def pprod.splus_def)
 
 lemma compute_sig_trd_spp_body_pprod [code]:
-  "sig_trd_spp_body_pprod cmp_pp bs v (p, r) =
-    (case find_sig_reducer_pprod cmp_pp bs v (lt_punit cmp_pp p) 0 of
-        None   \<Rightarrow> (tail_punit cmp_pp p, plus_monomial_less r (lc_punit cmp_pp p) (lt_punit cmp_pp p))
+  "sig_trd_spp_body_pprod to bs v (p, r) =
+    (case find_sig_reducer_pprod to bs v (lt_punit (proj_ord to) p) 0 of
+        None   \<Rightarrow> (tail_punit (proj_ord to) p, plus_monomial_less r (lc_punit (proj_ord to) p) (lt_punit (proj_ord to) p))
       | Some i \<Rightarrow> let b = snd (bs ! i) in
-          (tail_punit cmp_pp p - monom_mult_punit (lc_punit cmp_pp p / lc_punit cmp_pp b)
-              (lt_punit cmp_pp p - lt_punit cmp_pp b) (tail_punit cmp_pp b), r))"
+          (tail_punit (proj_ord to) p - monom_mult_punit (lc_punit (proj_ord to) p / lc_punit (proj_ord to) b)
+              (lt_punit (proj_ord to) p - lt_punit (proj_ord to) b) (tail_punit (proj_ord to) b), r))"
   by (simp add: plus_monomial_less_def split: option.split)
 
 lemma compute_sig_trd_spp_pprod [code]:
-  "sig_trd_spp_pprod cmp_pp bs (v, p) \<equiv> (v, sig_trd_spp_aux_pprod cmp_pp bs v (p, change_ord cmp_pp 0))"
+  "sig_trd_spp_pprod to bs (v, p) \<equiv> (v, sig_trd_spp_aux_pprod to bs v (p, change_ord (proj_ord to) 0))"
   by (simp add: change_ord_def)
 
 lemmas [code] = conversep_iff
 
-definition "sig_gb_pprod cmp_pp rword_strict fs \<equiv> sig_gb_pprod' cmp_pp (rword_strict cmp_pp) (map (change_ord cmp_pp) fs)"
+lemma POT_is_pot_ord: "pprod'.is_pot_ord (TYPE('a::nat)) (TYPE('b::nat)) (POT to)"
+  by (rule pprod'.is_pot_ordI, simp add: lt_of_nat_term_order nat_term_compare_POT pot_comp rep_nat_term_prod_def,
+      simp add: comparator_of_def)
 
-definition "zero_reds_pprod cmp_pp rword_strict fs0 =
-              (let fs = remdups (filter (\<lambda>f. f \<noteq> 0) fs0); Ksyz = Koszul_syz_sigs_pprod cmp_pp fs in
-                  length (fst (snd (sig_gb_spp_aux_pprod cmp_pp fs (rword_strict cmp_pp) ([], Ksyz, map Inr [0..<length fs])))) - length Ksyz)"
+definition "sig_gb_pprod to rword_strict fs \<equiv> sig_gb_pprod' to (rword_strict to) (map (change_ord (proj_ord to)) fs)"
+
+definition "zero_reds_pprod to rword_strict fs0 =
+              (let fs = remdups (filter (\<lambda>f. f \<noteq> 0) fs0); Ksyz = Koszul_syz_sigs_pprod to fs in
+                  length (fst (snd (sig_gb_spp_aux_pprod to fs (rword_strict to) ([], Ksyz, map Inr [0..<length fs])))) - length Ksyz)"
 
 lemma sig_gb_pprod'_eq_sig_gb_pprod:
-  "sig_gb_pprod' cmp_pp (rword_strict cmp_pp) fs = sig_gb_pprod cmp_pp rword_strict fs"
+  "sig_gb_pprod' to (rword_strict to) fs = sig_gb_pprod to rword_strict fs"
   by (simp add: sig_gb_pprod_def change_ord_def)
 
 thm pprod'.aux.sig_gb[OF pprod'.aux.rw_rat_strict_is_strict_rewrite_ord, simplified sig_gb_pprod'_eq_sig_gb_pprod]
 
-experiment begin interpretation trivariate\<^sub>0_rat .
-
 subsection \<open>Computations\<close>
+
+experiment begin interpretation trivariate\<^sub>0_rat .
 
 abbreviation "poly1 \<equiv> change_ord DRLEX (X\<^sup>2 * Z ^ 3 + 3 * X\<^sup>2 * Y)"
 abbreviation "poly2 \<equiv> change_ord DRLEX (X * Y * Z + 2 * Y\<^sup>2)"
@@ -276,9 +332,9 @@ value [code] "sig_gb_spp_body_pprod DRLEX ((cyclic DRLEX 2)::(_ \<Rightarrow>\<^
 
 value [code] "sig_gb_pprod DRLEX rw_rat_strict_pprod [poly1, poly2]"
 
-value [code] "timing (length (sig_gb_pprod DRLEX rw_rat_strict_pprod ((Katsura DRLEX 1)::(_ \<Rightarrow>\<^sub>0 rat) list)))"
+value [code] "timing (length (sig_gb_pprod (POT DRLEX) rw_rat_strict_pprod ((Katsura DRLEX 1)::(_ \<Rightarrow>\<^sub>0 rat) list)))"
 
-value [code] "timing (zero_reds_pprod DRLEX rw_rat_strict_pprod ((cyclic DRLEX 1)::(_ \<Rightarrow>\<^sub>0 rat) list))"
+value [code] "timing (zero_reds_pprod (POT DRLEX) rw_rat_strict_pprod ((cyclic DRLEX 1)::(_ \<Rightarrow>\<^sub>0 rat) list))"
 
 (* Timings on benchmark problems (on qftquad2)
 
